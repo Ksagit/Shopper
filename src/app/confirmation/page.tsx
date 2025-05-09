f782b7c50fa374afc98e5ab7eef5bd38e4d0f4cd
ksagit <kuba.kielaszek@gmail.com>
2025-05-10 01:08:10 +0200

    Early project structure

---
+++ src/app/confirmation/page.tsx
@@ -0,0 +1,7 @@
+export default function Confirmation() {
+  return (
+    <div>
+      <h1>hi from Confirmation</h1>
+    </div>
+  );
+}
---
+++ src/app/summary/page.tsx
@@ -0,0 +1,7 @@
+export default function Summary() {
+  return (
+    <div>
+      <h1>hi from Summary</h1>
+    </div>
+  );
+}
