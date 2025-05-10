b797c84513ed503b93b395f8986474a466650254
ksagit <kuba.kielaszek@gmail.com>
2025-05-10 16:56:39 +0200

    Config for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  basePath: "/Shopper",
+  images: { unoptimized: true },
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
